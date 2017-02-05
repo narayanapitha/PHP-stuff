18ccaf6954a63e6cc76e3be63db8755a22e97f2f
~#S*K#~ <srinivasKandukuri@users.noreply.github.com>
2017-02-05 14:07:28 +0530

    Updated

--- Interface/index.php
+++ Interface/index.php
@@ -1,39 +1,51 @@
 <?php
+interface ShapeInterface
+	{
+	public
 
-interface ShapeInterface{
-
-	public function area();
-
-}
-
-interface SolidShapeInterface{
-
-	public function volume();
-
-}
-
-interface ManageShapeInterface{
-
-	public function calculate();
-
-}
-
-class Cuboid implements ShapeInterface, SolidShapeInterface, ManageShapeInterface-{
-
-	public function area(){
-		
-		/*Do stuff here*/
-	
+	function area();
 	}
 
-	public function volume(){
-	
-		/*Do stuff here*/
-	
+interface SolidShapeInterface
+	{
+	public
+
+	function volume();
 	}
 
-	public function calculate(){
+interface ManageShapeInterface
+	{
+	public
+
+	function calculate();
+	}
+
+class Cuboid implements ShapeInterface, SolidShapeInterface, ManageShapeInterface
+
+	{
+	public
+
+	function area()
+		{
+		/*Do stuff here*/
+		return 10;
+		}
+
+	public
+
+	function volume()
+		{
+		/*Do stuff here*/
+		return $this->area() * 20;
+		}
+
+	public
+
+	function calculate()
+		{
 		return $this->area() + $this->volume();
+		}
 	}
-}
-?>
+
+$val = new Cuboid();
+echo $val->calculate();
